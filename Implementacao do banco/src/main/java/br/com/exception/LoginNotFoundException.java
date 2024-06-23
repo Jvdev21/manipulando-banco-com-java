6e7e35f0f713b2cd6f86119ae6ce0c52a88302f8
Joao V. Oliveira S <joao.vosilva19@aluno.educacao.pe.gov.br>
2024-06-23 13:07:23 -0300

    Delete Implementacao do banco directory

--- Implementacao do banco/src/main/java/br/com/exception/LoginNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.com.exception;
-
-public class LoginNotFoundException extends Exception{
-    public LoginNotFoundException(){
-    super("login nao encontrado no banco de dados");
-    }
-}
